647fcff05e268ff538c58d82bbcb861733da26f1
Michel Balazs <michel.balazs@gmail.com>
2023-12-16 11:53:57 -0300

    fix

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
+# -*- coding: utf-8 -*-
+from app import app
+import unittest
 
-from flask import Flask
-from flask_wtf.csrf import CSRFProtect
+class Test(unittest.TestCase):
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
 
-app = Flask(__name__)
-csrf = CSRFProtect(app)
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
 
-@app.route("/")
-def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(self.result.data.decode('utf-8'), "App do Balazs - v1")
